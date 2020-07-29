fdcbab0e0396e15fe949e7577d8c28723367125f
bobthin <bobthin@gmail.com>
2020-07-29 00:28:25 -0500

    20200729

---
+++ Data Science - Chapter 7 - Linear Regression.R
@@ -0,0 +1,387 @@
+## DATA SCIENCE ONLINE TRAINING - HARVARD edX
+
+#################################################################################
+## CHAPTER 7. LINEAR REGRESSION
+#################################################################################
+
+###############################
+## MONEYBALL CASE
+
+# Code: Scatterplot of the relationship between HRs and wins
+
+library(Lahman)
+library(tidyverse)
+library(dslabs)
+library(ggplot2)
+ds_theme_set()
+
+str(Teams)
+
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(HR_per_game = HR / G, R_per_game = R / G) %>%
+  ggplot(aes(HR_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
+
+# Code: Scatterplot of the relationship between stolen bases and wins
+
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(SB_per_game = SB / G, R_per_game = R / G) %>%
+  ggplot(aes(SB_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
+
+# Code: Scatterplot of the relationship between bases on balls and runs
+
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(BB_per_game = BB / G, R_per_game = R / G) %>%
+  ggplot(aes(BB_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
+
+# Other plots
+
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  ggplot(aes(AB, R)) + 
+  geom_point(alpha = 0.5)
+
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  mutate(AB_per_game = AB/G, R_per_game = R/G) %>%
+  ggplot(aes(AB_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
+
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  mutate(AB_per_game = AB/G, R_per_game = R/G) %>%
+  ggplot(aes(AB_per_game, R_per_game)) + 
+  geom_line()
+
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  mutate(AB_per_game = AB/G, R_per_game = R/G) %>%
+  ggplot(aes(R_per_game, AB_per_game)) + 
+  geom_point()
+
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  mutate(wins_per_game = W/G) %>%
+  ggplot(aes(wins_per_game, E)) + 
+  geom_point(alpha = 0.5)
+
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  ggplot(aes(X3B, X2B)) + 
+  geom_point(alpha = 0.5)
+
+###############################
+## CORRELATION
+
+# create the dataset
+library(tidyverse)
+library(HistData)
+data("GaltonFamilies")
+set.seed(1983)
+galton_heights <- GaltonFamilies %>%
+  filter(gender == "male") %>%
+  group_by(family) %>%
+  sample_n(1) %>%
+  ungroup() %>%
+  select(father, childHeight) %>%
+  rename(son = childHeight)
+
+# means and standard deviations
+galton_heights %>%
+  summarize(mean(father), sd(father), mean(son), sd(son))
+
+# scatterplot of father and son heights
+galton_heights %>%
+  ggplot(aes(father, son)) +
+  geom_point(alpha = 0.5)
+
+rho <- mean(scale(x)*scale(y))
+galton_heights %>% summarize(r = cor(father, son)) %>% pull(r)
+
+# compute sample correlation
+R <- sample_n(galton_heights, 25, replace = TRUE) %>%
+  summarize(r = cor(father, son))
+R
+
+# Monte Carlo simulation to show distribution of sample correlation
+B <- 1000
+N <- 25
+R <- replicate(B, {
+  sample_n(galton_heights, N, replace = TRUE) %>%
+    summarize(r = cor(father, son)) %>%
+    pull(r)
+})
+qplot(R, geom = "histogram", binwidth = 0.05, color = I("black"))
+
+# expected value and standard error
+mean(R)
+sd(R)
+
+# QQ-plot to evaluate whether N is large enough
+data.frame(R) %>%
+  ggplot(aes(sample = R)) +
+  stat_qq() +
+  geom_abline(intercept = mean(R), slope = sqrt((1-mean(R)^2)/(N-2)))
+
+
+#*******************************
+# Exercises
+
+# What is the correlation coefficient between number of runs per game and number of at bats per game?
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  summarize(r = cor(R/G, AB/G))
+library(Lahman)
+Teams_small <- Teams %>% filter(yearID %in% 1961:2001)
+cor(Teams_small$R/Teams_small$G, Teams_small$AB/Teams_small$G)
+
+# What is the correlation coefficient between win rate (number of wins per game) and number of errors per game?
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  summarize(r = cor(W/G, E/G))
+cor(Teams_small$W/Teams_small$G, Teams_small$E/Teams_small$G)
+
+# What is the correlation coefficient between doubles (X2B) per game and triples (X3B) per game?
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  summarize(r = cor(X2B/G, X3B/G))
+cor(Teams_small$X2B/Teams_small$G, Teams_small$X3B/Teams_small$G)
+
+###############################
+## Anscombe's Quartet/Stratification
+
+# number of fathers with height 72 or 72.5 inches
+sum(galton_heights$father == 72)
+sum(galton_heights$father == 72.5)
+
+# predicted height of a son with a 72 inch tall father
+conditional_avg <- galton_heights %>%
+  filter(round(father) == 72) %>%
+  summarize(avg = mean(son)) %>%
+  pull(avg)
+conditional_avg
+
+# stratify fathers' heights to make a boxplot of son heights
+galton_heights %>% mutate(father_strata = factor(round(father))) %>%
+  ggplot(aes(father_strata, son)) +
+  geom_boxplot() +
+  geom_point(alpha = 0.5)
+
+# center of each boxplot
+galton_heights %>%
+  mutate(father = round(father)) %>%
+  group_by(father) %>%
+  summarize(son_conditional_avg = mean(son)) %>%
+  ggplot(aes(father, son_conditional_avg)) +
+  geom_point()
+
+# example
+r <- galton_heights %>% summarize(r = cor(father, son)) %>% .$r
+galton_heights %>%
+  mutate(father = round(father)) %>%
+  group_by(father) %>%
+  summarize(son = mean(son)) %>%
+  mutate(z_father = scale(father), z_son = scale(son)) %>%
+  ggplot(aes(z_father, z_son)) +
+  geom_point() +
+  geom_abline(intercept = 0, slope = r)
+
+# calculate values to plot regression line on original data - IMPORTANT
+mu_x <- mean(galton_heights$father)
+mu_y <- mean(galton_heights$son)
+s_x <- sd(galton_heights$father)
+s_y <- sd(galton_heights$son)
+r <- cor(galton_heights$father, galton_heights$son)
+m <- r * s_y/s_x
+b <- mu_y - m*mu_x
+
+# add regression line to plot
+galton_heights %>%
+  ggplot(aes(father, son)) +
+  geom_point(alpha = 0.5) +
+  geom_abline(intercept = b, slope = m)
+
+# y = b + mx
+# When two variables follow a bivariate normal distribution, the variation explained can be calculated as  𝜌2×100 .
+
+vignette("ggplot2-specs") # help on aesthetics of ggplot
+
+galton_heights %>%
+  mutate(z_father = round((father - mean(father)) / sd(father))) %>%
+  filter(z_father %in% -2:2) %>%
+  ggplot() +  
+  stat_qq(aes(sample = son)) +
+  facet_wrap( ~ z_father)
+
+# regression lines both ways
+
+# compute a regression line to predict the son's height from the father's height
+mu_x <- mean(galton_heights$father)
+mu_y <- mean(galton_heights$son)
+s_x <- sd(galton_heights$father)
+s_y <- sd(galton_heights$son)
+r <- cor(galton_heights$father, galton_heights$son)
+m_1 <-  r * s_y / s_x
+b_1 <- mu_y - m_1*mu_x
+# add regression line to plot
+galton_heights %>%
+  ggplot(aes(son, father)) +
+  geom_point(alpha = 0.5) +
+  geom_abline(intercept = b_1, slope = m_1)
+
+
+# compute a regression line to predict the father's height from the son's height
+m_2 <-  r * s_x / s_y
+b_2 <- mu_x - m_2*mu_y
+# add regression line to plot
+galton_heights %>%
+  ggplot(aes(father, son)) +
+  geom_point(alpha = 0.5) +
+  geom_abline(intercept = b_2, slope = m_2)
+
+
+#*******************************
+# Assessment
+
+set.seed(1989) #if you are using R 3.5 or earlier
+set.seed(1989, sample.kind="Rounding") #if you are using R 3.6 or later
+library(HistData)
+data("GaltonFamilies")
+
+female_heights <- GaltonFamilies%>%     
+  filter(gender == "female") %>%     
+  group_by(family) %>%     
+  sample_n(1) %>%     
+  ungroup() %>%     
+  select(mother, childHeight) %>%     
+  rename(daughter = childHeight)
+
+mum <- mean(female_heights$mother)
+sdm <- sd(female_heights$mother)
+mud <- mean(female_heights$daughter)
+sdd <- sd(female_heights$daughter)
+r <- cor(female_heights$mother, female_heights$daughter)
+m <- r * sdd/sdm
+b <-  mud - m*mum 
+
+# Calculate the slope and intercept of the regression line predicting daughters' heights given mothers' heights. 
+#Given an increase in mother's height by 1 inch, how many inches is the daughter's height expected to change?
+
+#Slope of regression line predicting daughters' height from mothers' heights
+r * sdd/sdm
+
+#Intercept of regression line predicting daughters' height from mothers' heights
+mud - (r * sdd/sdm)*mum
+
+#Change in daughter's height in inches given a 1 inch increase in the mother's height
+r * sdd/sdm
+
+#What percent of the variability in daughter heights is explained by the mother's height?
+r^2
+
+#A mother has a height of 60 inches. What is the conditional expected value of her daughter's height given the mother's height?
+x <- 60
+y = b + m*x
+y
+
+
+###############################
+## LINEAR MODELS
+
+# find regression line for predicting runs from BBs
+library(tidyverse)
+library(Lahman)
+bb_slope <- Teams %>% 
+  filter(yearID %in% 1961:2001 ) %>% 
+  mutate(BB_per_game = BB/G, R_per_game = R/G) %>% 
+  lm(R_per_game ~ BB_per_game, data = .) %>% 
+  .$coef %>%
+  .[2]
+bb_slope
+
+# compute regression line for predicting runs from singles
+singles_slope <- Teams %>% 
+  filter(yearID %in% 1961:2001 ) %>%
+  mutate(Singles_per_game = (H-HR-X2B-X3B)/G, R_per_game = R/G) %>%
+  lm(R_per_game ~ Singles_per_game, data = .) %>%
+  .$coef  %>%
+  .[2]
+singles_slope
+
+# calculate correlation between HR, BB and singles
+Teams %>% 
+  filter(yearID %in% 1961:2001 ) %>% 
+  mutate(Singles = (H-HR-X2B-X3B)/G, BB = BB/G, HR = HR/G) %>%  
+  summarize(cor(BB, HR), cor(Singles, HR), cor(BB,Singles))
+
+###############################
+## STRATIFICATION ANN MULTIVARIATE REGRESSION
+
+
+# stratify HR per game to nearest 10, filter out strata with few points
+dat <- Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(HR_strata = round(HR/G, 1), 
+         BB_per_game = BB / G,
+         R_per_game = R / G) %>%
+  filter(HR_strata >= 0.4 & HR_strata <=1.2)
+
+# scatterplot for each HR stratum
+dat %>% 
+  ggplot(aes(BB_per_game, R_per_game)) +  
+  geom_point(alpha = 0.5) +
+  geom_smooth(method = "lm") +
+  facet_wrap( ~ HR_strata)
+
+# calculate slope of regression line after stratifying by HR
+dat %>%  
+  group_by(HR_strata) %>%
+  summarize(slope = cor(BB_per_game, R_per_game)*sd(R_per_game)/sd(BB_per_game))
+
+# stratify by BB
+dat <- Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(BB_strata = round(BB/G, 1), 
+         HR_per_game = HR / G,
+         R_per_game = R / G) %>%
+  filter(BB_strata >= 2.8 & BB_strata <=3.9) 
+
+# scatterplot for each BB stratum
+dat %>% ggplot(aes(HR_per_game, R_per_game)) +  
+  geom_point(alpha = 0.5) +
+  geom_smooth(method = "lm") +
+  facet_wrap( ~ BB_strata)
+
+# slope of regression line after stratifying by BB
+dat %>%  
+  group_by(BB_strata) %>%
+  summarize(slope = cor(HR_per_game, R_per_game)*sd(R_per_game)/sd(HR_per_game)) 
+
+###############################
+## LSE - LEAST SQUARES ESTIMATES
+
+# compute RSS for any pair of beta0 and beta1 in Galton's data
+library(HistData)
+data("GaltonFamilies")
+set.seed(1983)
+galton_heights <- GaltonFamilies %>%
+  filter(gender == "male") %>%
+  group_by(family) %>%
+  sample_n(1) %>%
+  ungroup() %>%
+  select(father, childHeight) %>%
+  rename(son = childHeight)
+rss <- function(beta0, beta1, data){
+  resid <- galton_heights$son - (beta0+beta1*galton_heights$father)
+  return(sum(resid^2))
+}
+
+# plot RSS as a function of beta1 when beta0=25
+beta1 = seq(0, 1, len=nrow(galton_heights))
+results <- data.frame(beta1 = beta1,
+                      rss = sapply(beta1, rss, beta0 = 25))
+results %>% ggplot(aes(beta1, rss)) + geom_line() + 
+  geom_line(aes(beta1, rss))
+
+#*******************************
+#*******************************
+#*******************************
+
+
+
+
+###############################
+##
+###############################
+##
